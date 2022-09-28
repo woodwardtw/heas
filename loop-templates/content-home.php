166a50a2efd11a44641185b9a0f1bf784f0e2756
woodwardtw <twwoodward@me.com>
2022-09-28 11:22:34 -0400

    home page work

--- loop-templates/content-home.php
+++ loop-templates/content-home.php
@@ -21,11 +21,9 @@ defined( 'ABSPATH' ) || exit;
 	<div class="entry-content">
 		<div class="row">
 		    <div class="col-md-6">
-		      <div class="text-block left">
 		      	<?php echo heaf_about();?>
 		      	<?php echo heaf_positions();?>
 				<?php echo heaf_questions();?>		      	
-		      </div>
 		    </div>
 		 	<!--end col md 6-->
 		    <div class="col-md-6">
@@ -34,6 +32,55 @@ defined( 'ABSPATH' ) || exit;
 		        <div id="home-posts"></div>
 		      </div>
 		    </div>
+		 </div>
+		  <!--end row-->
+		  <div class="row">
+		    <div class="col-md-6">
+		      <div class="text-block left">
+		          <h2 id="people">People</h2>        
+		      </div>
+		    </div>
+		    <div class="col-md-6">
+		     <div class="text-block left">
+		          <h2 id="participate">Participate</h2> 
+		       <p>Call to action. Contact or something like that.</p>
+		      </div>
+		      <svg xmlns="http://www.w3.org/2000/svg">
+		           <filter id="filter" x="0" y="0">
+		        <feFlood x="4" y="4" height="2" width="2"/>    
+		        <feComposite width="10" height="10"/>
+		        <feTile result="a"/>
+		        <feComposite in="SourceGraphic" in2="a" operator="in"/>
+		        <feMorphology operator="dilate" radius="5"/>
+		      </filter>
+		      </svg>
+		    </div> 
+		    <div class="col-md-3">      
+		            <!-- use the  #filter filter -->
+		      <div class="bio holder">
+		            <img src="https://dlinq.middcreate.net/wp-content/uploads/2021/07/Woodward_Tom_325_420.jpg"  alt="Blurred bio image.">
+		      </div>  
+		              <h3>Tom Woodward</h3>
+		    </div>
+		      <div class="col-md-3">
+		        <div class="bio holder">
+		            <img src="https://dlinq.middcreate.net/wp-content/uploads/2021/07/Woodward_Tom_325_420.jpg"  alt="Blurred bio image.">        
+		        </div>     
+		        <h3>Tom Woodward</h3>
+		      </div>
+		        <div class="col-md-3">
+		        <div class="bio holder">
+		            <img src="https://dlinq.middcreate.net/wp-content/uploads/2021/07/Woodward_Tom_325_420.jpg"  alt="Blurred bio image.">
+		        </div>  
+		        <h3>Tom Woodward</h3>
+		      </div>    
+		          <div class="col-md-3">
+		        <div class="bio holder">
+		            <img src="https://dlinq.middcreate.net/wp-content/uploads/2021/07/Woodward_Tom_325_420.jpg"  alt="Blurred bio image.">
+		        </div> 
+		       <h3>Tom Woodward</h3>
+		      </div>
+		    </div>  
 		  </div>
 		  <!--end row-->
 
